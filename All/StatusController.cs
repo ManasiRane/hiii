62c1f9dcb59f74da75fefc108a74a721cbea3d9f
ManasiRane <maurane8353@gmail.com>
2020-02-18 14:50:10 +0530

    initiate commit

---
+++ All/StatusController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJIVE.Controllers
+{
+    public class StatusController : Controller
+    {
+        // GET: Status
+        public ActionResult Status()
+        {
+            return View();
+        }
+    }
+}
---
+++ All/TicketTypeController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PROJIVE.Controllers
+{
+    public class TicketTypeController : Controller
+    {
+        // GET: TicketType
+        public ActionResult TicketType()
+        {
+            return View();
+        }
+    }
+}
